b4f0d3d7063b509174ec4279deadeb25bb0ae05b
jogm08 <josuegomezm0808@gmail.com>
2024-02-29 11:49:37 -0600

    PrimeraSemanaRepaso

---
+++ Documents/NetBeansProjects/CasoPractico1/TiendaVi/src/main/java/Tinda_Vi/demo/TiendaViApplication.java
@@ -0,0 +1,13 @@
+package Tinda_Vi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaViApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaViApplication.class, args);
+	}
+
+}
---
+++ Documents/NetBeansProjects/CasoPractico1/TiendaVi/src/test/java/Tinda_Vi/demo/TiendaViApplicationTests.java
@@ -0,0 +1,13 @@
+package Tinda_Vi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaViApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
