07d74732948b29dafd53c03e20375d67efa172be
jogm08 <josuegomezm0808@gmail.com>
2024-02-28 21:06:25 -0600

    Este es el primer Commit creando el proyecto Boost

---
+++ src/main/java/CasoGrupal1/demo/CasoGrupal1Application.java
@@ -0,0 +1,13 @@
+package CasoGrupal1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoGrupal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoGrupal1Application.class, args);
+	}
+
+}
